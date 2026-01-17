fb2829d8349817a11999e061353a1cc26d78859e
Joao-Assis1 <joao_assis_santos@proton.me>
2026-01-17 14:35:47 -0400

    refactor: :recycle: (frontend): remove vue 3 application codebase

--- frontend/sistema-sorteios-front/src/main.js
+++ frontend/sistema-sorteios-front/src/main.js
@@ -1,13 +1,6 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
 
-import App from './App.vue'
-import router from './router'
-import './assets/main.css'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).mount("#app");
