15d95edd1b0281e2b036c7ff02aba422afc841f6
Joao-Assis1 <joao_assis_santos@proton.me>
2026-01-18 15:15:55 -0400

    refactor: :recycle: Refactoring the frontend connection with the backend

--- frontend/sistema-sorteios-front/src/services/api.js
+++ frontend/sistema-sorteios-front/src/services/api.js
@@ -1,10 +1,32 @@
 import axios from "axios";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ewbctljhkxhhawflfogy.supabase.co";
+const supabaseKey = "sb_publishable_CzPALNStOxmqZh7GXE5z2g_91G578V_";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://sistema-sorteios.onrender.com",
 });
+
+// O INTERCEPTADOR MÁGICO 🧙‍♂️
+api.interceptors.request.use(
+  async (config) => {
+    // 1. Pega a sessão atual do usuário no navegador
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    // 2. Se tiver sessão (usuário logado), anexa o Token
+    if (session?.access_token) {
+      config.headers.Authorization = `Bearer ${session.access_token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
