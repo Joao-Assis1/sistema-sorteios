30a2db188b7c195396e954060e7b059ca698bfc2
Joao-Assis1 <joao_assis_santos@proton.me>
2026-01-23 19:58:51 -0400

    refactor: :recycle: Fixing exposed credentials error

--- frontend/sistema-sorteios-front/src/services/supabase.js
+++ frontend/sistema-sorteios-front/src/services/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = "https://ewbctljhkxhhawflfogy.supabase.co";
+const supabaseKey = "sb_publishable_CzPALNStOxmqZh7GXE5z2g_91G578V_";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
